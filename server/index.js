aff01dc81b02c6d36b3356ece2ca08abc7577cde
unknown <awalupshot@gmail.com>
2022-08-03 14:49:26 +0545

    first commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import postRoutes from "./routes/posts.js"
+
+const app = express();
+dotenv.config();
+
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, {
+        maxPoolSize: 50,
+        wtimeoutMS: 2500,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => app.listen(PORT, () => console.log(`server is connected on: ${PORT}`)))
+    .catch((error) => console.log(error.message));
